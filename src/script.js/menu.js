83ab90cbe6092e90eb6e911c1594b9511571ac06
LandewankFF <landewankfahrezaf@gmail.com>
2023-11-19 23:39:06 -0800

    tinggal view work

--- src/script.js/menu.js
+++ src/script.js/menu.js
@@ -1,7 +1,7 @@
-// const hamburgerIcon = document.getElementById('hamburgericon');
-// const navigation = document.querySelector('.navigation');
+const hamburgerIcon = document.getElementById('hamburgericon');
+const navigation = document.querySelector('.navigation');
 
-// function toggleMenu() {
-//     navigation.classList.toggle('active');
-// }
-// hamburgerIcon.addEventListener('click', toggleMenu);
+function toggleMenu() {
+    navigation.classList.toggle('active');
+}
+hamburgerIcon.addEventListener('click', toggleMenu);
